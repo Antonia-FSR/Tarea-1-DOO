c74a9f528b2b712063e15a899a958b74f6de7076
Antonia-FSR <antoniasando123r@gmail.com>
2026-04-22 23:34:17 -0400

    Se crea familia de Productos

---
+++ src/main/java/CocaCola.java
@@ -0,0 +1,9 @@
+public class CocaCola extends Bebida{
+
+    public CocaCola(int serie){
+        super(serie);
+    }
+    public String Consumir(){
+        return "CocaCola";
+    }
+}
---
+++ src/main/java/Fanta.java
@@ -0,0 +1,8 @@
+public class Fanta extends Bebida{
+    public Fanta(int serie){
+        super(serie);
+    }
+    public String Consumir(){
+        return "Fanta";
+    }
+}
