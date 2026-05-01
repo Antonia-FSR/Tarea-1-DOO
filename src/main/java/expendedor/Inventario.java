18303e076a72c57b98ffcd97e6fac2acfdc3f61c
jaocSec <jaoc2001@hotmail.com>
2026-05-01 17:13:19 -0400

    refactor: Agregar constantes de selección a cada elemento

--- src/main/java/expendedor/Inventario.java
+++ src/main/java/expendedor/Inventario.java
@@ -1,16 +1,23 @@
+package expendedor;
+
 public enum Inventario {
-    COCA_COLA(1000),
-    SPRITE(1000),
-    FANTA(1000),
-    SNICKERS(800),
-    SUPER8(600);
+    COCA_COLA(1000, 1),
+    SPRITE(1000, 2),
+    FANTA(1000, 3),
+    SNICKERS(800, 4),
+    SUPER8(600, 5);
 
     private final int precio;
-    Inventario(int precio){
+    private final int seleccion;
+
+    Inventario(int precio, int seleccion){
+
         this.precio=precio;
+        this.seleccion=seleccion;
     }
 
     public int getPrecio() {
         return precio;
     }
+    public int getSeleccion() {return seleccion;}
 }
