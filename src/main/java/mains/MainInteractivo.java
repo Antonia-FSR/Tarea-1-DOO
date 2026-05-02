a5fa27481aedef37789c8465f4241159ccb02ad8
9ghtBug <mailforcoolleg@gmail.com>
2026-05-01 20:27:41 -0400

    feat: Clase MainInteractivo y Clase MainPruebas agregadas

---
+++ src/main/java/mains/MainInteractivo.java
@@ -0,0 +1,12 @@
+package mains;
+
+import expendedor.Expendedor;
+import comprador.Comprador;
+import monedas.*;
+import java.util.Scanner;
+
+public class MainInteractivo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/mains/MainPruebas.java
@@ -0,0 +1,15 @@
+package mains;
+
+import expendedor.Expendedor;
+import comprador.Comprador;
+import monedas.*;
+import excepciones.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MainPruebas {
+    public static void main(String[] args) {
+
+    }
+}
